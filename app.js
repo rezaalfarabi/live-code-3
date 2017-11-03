c1e3a707b4348e3fd2bb1decc215866b8fb1f4a7
rezaalfarabi <anangreza18@gmail.com>
2017-11-03 09:41:13 +0700

    livecode3

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //your code here
+const express = require('express')
+const app = express()
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/movies', function (req, res) {
+    res.render('movies')
+  })
+
+  app.get('/prodHouses', function (req, res) {
+    res.render('prodHouses')
+  })
+
+app.listen(3000, function () {
+  console.log('test port 3000')
+})
+
+
